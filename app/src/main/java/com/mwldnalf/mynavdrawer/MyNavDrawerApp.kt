e9e7505e2fa7a8272f2a8d06d659618ee61e2d65
wilelilac <wildfahreza@gmail.com>
2023-11-15 00:25:18 +0700

    first commit

---
+++ app/src/main/java/com/mwldnalf/mynavdrawer/MyNavDrawerApp.kt
@@ -0,0 +1,159 @@
+package com.mwldnalf.mynavdrawer
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mwldnalf.mynavdrawer.ui.theme.MyNavDrawerTheme
+import kotlinx.coroutines.launch
+
+data class MenuItem(val title: String, val icon: ImageVector)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyNavDrawerApp() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    val context = LocalContext.current
+
+    val items = listOf(
+        MenuItem(
+            title = stringResource(R.string.home),
+            icon = Icons.Default.Home
+        ),
+        MenuItem(
+            title = stringResource(R.string.favourite),
+            icon = Icons.Default.Favorite
+        ),
+        MenuItem(
+            title = stringResource(R.string.profile),
+            icon = Icons.Default.AccountCircle
+        ),
+    )
+
+    val selectedItem = remember { mutableStateOf(items[0]) }
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+        topBar = {
+            MyTopBar(
+                onMenuClick = {
+                    scope.launch {
+                        if (drawerState.isClosed) {
+                            drawerState.open()
+                        } else {
+                            drawerState.close()
+                        }
+                    }
+                }
+            )
+        },
+    ) { paddingValues ->
+        ModalNavigationDrawer(
+            modifier = Modifier.padding(paddingValues),
+            drawerState = drawerState,
+            gesturesEnabled = drawerState.isOpen,
+            drawerContent = {
+                ModalDrawerSheet {
+                    Spacer(Modifier.height(12.dp))
+                    items.forEach { item ->
+                        NavigationDrawerItem(
+                            icon = { Icon(item.icon, contentDescription = null) },
+                            label = { Text(item.title) },
+                            selected = item == selectedItem.value,
+                            onClick = {
+                                scope.launch {
+                                    drawerState.close()
+                                    snackbarHostState.showSnackbar(
+                                        message = context.resources.getString(R.string.coming_soon, item.title),
+                                        actionLabel = context.resources.getString(R.string.subscribe_question),
+                                        withDismissAction = true,
+                                        duration = SnackbarDuration.Short
+                                    )
+                                }
+                                selectedItem.value = item
+                            },
+                            modifier = Modifier.padding(horizontal = 12.dp)
+                        )
+                    }
+                }
+            },
+            content = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        if (drawerState.isClosed) {
+                            stringResource(R.string.swipe_to_open)
+                        } else {
+                            stringResource(R.string.swipe_to_close)
+                        }
+                    )
+                }
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopBar(onMenuClick: () -> Unit) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = {
+                onMenuClick()
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = stringResource(R.string.menu)
+                )
+            }
+        },
+        title = {
+            Text(stringResource(R.string.app_name))
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyNavDrawerTheme {
+        MyNavDrawerApp()
+    }
+}
